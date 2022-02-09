c29be565b7cce90e5ea9a2410c240d5176912bdd
Gavin Bosman <gavin.bosman@ontariotechu.net>
2022-02-08 20:13:34 -0500

    rebuilt lab_02 for gradle

--- lab_02/src/main/java/csci2020u/lab_02/SimpleTest.java
+++ lab_02/src/main/java/csci2020u/lab_02/SimpleTest.java
@@ -1,7 +1,7 @@
 package csci2020u.lab_02;
 
-class SimpleTest {
-    public static void main(String[] args){
-        System.out.println("Hello World, this is my first gradle program!");
-    }
+public class SimpleTest{
+	public static void main(String[] args){
+		System.out.println("This is my first Gradle project!");
+	}
 }
