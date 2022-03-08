a372300f3aadccad46e34b44ccf4f478f0a28c60
Gavin Bosman <gavin.bosman@ontariotechu.net>
2022-03-08 13:03:50 -0500

    reformatted lab-05, added screenshots

--- Lab_05/src/main/java/com/example/lab_05/StudentTable.java
+++ Lab_05/src/main/java/com/example/lab_05/StudentTable.java
@@ -10,8 +10,6 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 import java.io.IOException;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
 public class StudentTable extends Application {
 
@@ -21,7 +19,7 @@ public class StudentTable extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-
+        DataSource dataSource = new DataSource();
         primaryStage.setTitle("CSCI2020U - Lab 05");
 
         //Student ID column
@@ -57,7 +55,7 @@ public class StudentTable extends Application {
 
 
         TableView<StudentRecord> table = new TableView<>();
-        table.setItems(DataSource.getAllMarks());
+        table.setItems(dataSource.getAllMarks());
         table.getColumns().addAll(idColumn, assignmentColumn, midtermColumn, finalColumn, totalColumn, letterColumn);
 
         VBox vbox = new VBox();
@@ -70,23 +68,3 @@ public class StudentTable extends Application {
     }
 }
 
-class DataSource {
-    public static ObservableList<StudentRecord> getAllMarks() {
-        ObservableList<StudentRecord> marks =
-                FXCollections.observableArrayList();
-
-// Student ID, Assignments, Midterm, Final exam
-        marks.add(new StudentRecord("100100100", 75.0f, 68.0f, 54.25f));
-        marks.add(new StudentRecord("100100101", 70.0f, 69.25f, 51.5f));
-        marks.add(new StudentRecord("100100102", 100.0f, 97.0f, 92.5f));
-        marks.add(new StudentRecord("100100103", 90.0f, 88.5f, 68.75f));
-        marks.add(new StudentRecord("100100104", 72.25f, 74.75f, 58.25f));
-        marks.add(new StudentRecord("100100105", 85.0f, 56.0f, 62.5f));
-        marks.add(new StudentRecord("100100106", 70.0f, 66.5f, 61.75f));
-        marks.add(new StudentRecord("100100107", 55.0f, 47.0f, 50.5f));
-        marks.add(new StudentRecord("100100108", 40.0f, 32.5f, 27.75f));
-        marks.add(new StudentRecord("100100109", 82.5f, 77.0f, 74.25f));
-
-        return marks;
-    }
-}
